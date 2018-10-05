4a6340823147acb525fe3e111ca972247743af6d
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2018-10-05 11:35:56 +0200

    Examples of Python Codes

---
+++ Python/example_control_if.py
@@ -0,0 +1,13 @@
+input_word = raw_input("Enter a name: ")
+
+if len(input_word) == 6:
+    print("Length of the word is six")
+
+
+elif len(input_word) > 6:
+    print ("Length of the word is more than six")
+
+else:
+    print ("Length of word is less than six")
+
+print("Done")
