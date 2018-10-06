9233c95d6193b6a21078b5134d3cbef1e0cb4942
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2018-10-06 10:46:46 +0200

    python

--- Python/example_length_words.py
+++ Python/example_length_words.py
@@ -2,7 +2,5 @@ a = "Hello how are you , what are you doing"
 b = a.split(' ')
 def len_str(b):
     for x in b:
-        if(len(x)%2 !=0):
-            print(x)
-
+        print(x, len(x))
 len_str(b)
