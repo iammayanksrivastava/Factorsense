2cfaddedb741790c24b8682e467afe63bdfcd8a5
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2018-10-13 00:00:30 +0200

    Changes to param file
    Signed-off-by: Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>

--- Python/sqoop_oracle_hdfs.py
+++ Python/sqoop_oracle_hdfs.py
@@ -4,19 +4,9 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 import getpass
 import json
 
-
-#Read the parameters from the json file 
-#with open(tables.json) as json_param_file:
-#    tables = json.load(json_param_file)
-#print data[tables]
-
 #Read the parameters from the json file 
 with open('tables.json') as json_param_file:
     table_name = json.load(json_param_file)
-
-src_tables = table_name['source_tables']
-
-print(src_tables)
 
 # Function to run Hadoop command
 def run_unix_cmd(args_list):
@@ -30,6 +20,7 @@ def run_unix_cmd(args_list):
 def sqoop_job(table_name):
     cmd = ['sqoop', 'import', '--connect', 'jdbc:oracle:thin:@sl09.atradiusnet.com:1519/SYMF.atradiusnet.com', '--username', 'NLSMAY1','--password', 'Winter18','--table', 'ORABUP0.'+table_name, '-m', '1', '--hive-import', '--hive-table', 'D_NATIVE_ZONE.'+table_name]
     print(cmd)
+    
     (ret, out, err) = run_unix_cmd(cmd)
     print(ret, out, err)
     if ret == 0:
@@ -41,5 +32,5 @@ def sqoop_job(table_name):
   ##  for k, values in table_name.items:
     ##    sqoop_job(table_name)
 
-for i in src_tables:
+for i in table_name:
     sqoop_job(i)
