db83eb896fbf07d99affc606871f47a5503cd985
nlsmay1 <mayanksrivastava@Mayanks-MacBook-Pro-2.local>
2018-10-25 15:11:48 +0200

    Added Folder for Twitter Analytics

--- Python/Twitter Analytics/twitter_stream.py
+++ Python/Twitter Analytics/twitter_stream.py
@@ -4,7 +4,7 @@ import sys
 import codecs
 import json
 from tweepy import Stream
-from tweepy.streaming import StreamListener
+from tweepy import StreamListener
 from tweepy import OAuthHandler
 
 #Credentials
@@ -17,16 +17,16 @@ access_secret = ''
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 #Input file for
-filename = raw_input("\n Enter the name of output file:")
+filename = input("\n Enter the name of output file:")
 
 
 #Information for the user:
-print "Please wait. Connecting and authenticating now..."
-print "#########################################################################"
-print "Please note that search keywords are hard-wired until further"
-print "notice. If you want to change them, you will have to open the program"
-print "file and change them there!"
-print "#########################################################################\n"
+print ("Please wait. Connecting and authenticating now...")
+print ("#########################################################################")
+print ("Please note that search keywords are hard-wired until further")
+print ("notice. If you want to change them, you will have to open the program")
+print ("file and change them there!")
+print ("#########################################################################\n")
 
 class listener(StreamListener):
 
@@ -38,7 +38,7 @@ class listener(StreamListener):
             text = jsonData['text']
             saveThis = createdAt+'=>' +text
 
-            print savethis
+            print ("savethis")
 
             saveFile = open(filename+' .csv', 'a')
 
@@ -47,12 +47,12 @@ class listener(StreamListener):
             saveFile.close()
             return True
 
-        except BaseException, e:
-            print 'failed ondata,  ', str(e)
+        except BaseException:
+            print ('failed ondata)
             time.sleep(5)
 
 def on_error(self, status):
-    print status
+    print (status)
 
 # The meat of the script, authentication first, then streaming
 auth = OAuthHandler(consumer_key, consumer_secret)
