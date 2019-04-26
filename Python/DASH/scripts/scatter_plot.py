7f6f229ead1513fb520c125e875ad2383260d7f6
Mayank Srivastava <mayanksrivastava@Mayanks-MacBook-Pro.local>
2019-04-26 14:53:02 +0200

    Plotly Training

--- Python/DASH/scripts/scatter_plot.py
+++ Python/DASH/scripts/scatter_plot.py
@@ -1,12 +1,22 @@
 import numpy as np
-import plotly.offline as plot
-import plotly.graph_objs as go 
+import plotly.offline as pyo
+from plotly import graph_objs as go
 
 np.random.seed(42)
 
 random_x = np.random.randint(1,101, 100)
 random_y = np.random.randint(1,101, 100)
 
-data = [go.Scatter(x= random_x, y=random_y, mode='markers')]
+data = [go.Scatter(x= random_x, y=random_y, mode='markers',
+                    marker=dict(
+                       size=8,
+                       color='rgb(51,204,153)',
+                       symbol='circle',
+                       line = {'width':.5}
+                   ) )]
 
-plot.plot(data, filename='scatter.html')
+layout = go.Layout(title='Random Number Scatter Plot', xaxis= dict(title ='X Axis'), yaxis=dict(title = 'Y Axis'), hovermode='closest')
+
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename='scatter.html')
