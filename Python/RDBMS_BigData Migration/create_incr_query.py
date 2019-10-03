f2cd7e168a1938bc402064d55cd14ad11079ec3c
Mayank Srivastava <mayank.srivastav84@gmail.com>
2019-10-03 09:33:00 +0200

    Update create_incr_query.py

--- Python/RDBMS_BigData Migration/create_incr_query.py
+++ Python/RDBMS_BigData Migration/create_incr_query.py
@@ -7,7 +7,7 @@ import getpass
 import json
 
 #Declare Connection Parameter here
-connstr='NLSMAY1/Winter18@sl09.atradiusnet.com:1519/SYMF.atradiusnet.com:ORABUP0'
+connstr=''
 conn = cx_Oracle.connect(connstr)
 cur = conn.cursor()
 
